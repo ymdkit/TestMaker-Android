a558b9277bc68dbd98a873e3dc8db7eff0a41115
山田敬汰 <banira0428@gmail.com>
2018-08-30 17:47:34 +0900

    airより

--- app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/activities/BaseActivity.java
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/activities/BaseActivity.java
@@ -20,7 +20,7 @@ import jp.gr.java_conf.foobar.testmaker.service.models.RealmController;
  * Created by keita on 2016/08/19.
  */
 
-//edited by macBook pro
+//edited by macBook air
 
 public class BaseActivity extends AppCompatActivity {
 
