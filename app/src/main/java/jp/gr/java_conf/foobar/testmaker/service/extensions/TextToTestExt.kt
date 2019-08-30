de468004fd442d4c544bbfd64111e95d2622684e
keita <banira0428@gmail.com>
2019-08-30 09:13:17 +0900

    コピペで読み込む機能　修正

--- app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/extensions/TextToTestExt.kt
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/extensions/TextToTestExt.kt
@@ -6,11 +6,14 @@ import jp.gr.java_conf.foobar.testmaker.service.R
 import jp.gr.java_conf.foobar.testmaker.service.models.Quest
 import jp.gr.java_conf.foobar.testmaker.service.models.Test
 
-fun String.toTest(context: Context,questionId: Long): Test {
+fun String.toTest(context: Context, questionId: Long): Test {
 
     val backups = split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
 
     val test = Test()
+    test.title = context.getString(R.string.unknown)
+    test.color = context.resources.getIntArray(R.array.color_list)[0]
+
 
     var resultNumber = 0
 
@@ -38,80 +41,75 @@ fun String.toTest(context: Context,questionId: Long): Test {
                     resultNumber += 1
 
                 } else if (backup[0] == context.getString(R.string.load_multiple_answers)) {
-                    if (backup.size - 2 <= Constants.ANSWER_MAX) {
+                    if (backup.size - 2 > Constants.ANSWER_MAX) continue
 
-                        question.setAnswers(backup.drop(2).toTypedArray())
-                        backup.drop(2).toTypedArray().forEach { question.answer += "$it " }
-                        question.type = Constants.COMPLETE
+                    question.setAnswers(backup.drop(2).toTypedArray())
+                    backup.drop(2).toTypedArray().forEach { question.answer += "$it " }
+                    question.type = Constants.COMPLETE
 
-                        resultNumber += 1
-
-                    }
+                    resultNumber += 1
 
                 } else if (backup[0] == context.getString(R.string.load_multiple_answers_order)) {
-                    if (backup.size - 2 <= Constants.ANSWER_MAX) {
+                    if (backup.size - 2 > Constants.ANSWER_MAX) continue
 
-                        question.setAnswers(backup.drop(2).toTypedArray())
-                        question.isCheckOrder = true
-                        question.type = Constants.COMPLETE
+                    question.setAnswers(backup.drop(2).toTypedArray())
+                    question.isCheckOrder = true
+                    question.type = Constants.COMPLETE
 
-                        resultNumber += 1
+                    resultNumber += 1
 
-                    }
 
                 } else if (backup[0] == context.getString(R.string.load_selection_problems)) {
 
-                    if (backup.size - 3 <= Constants.OTHER_SELECT_MAX) {
+                    if (backup.size - 3 > Constants.OTHER_SELECT_MAX) continue
 
+                    question.answer = backup[2]
+                    question.setSelections(backup.drop(3).toTypedArray())
+                    question.type = Constants.SELECT
 
-                        question.answer = backup[2]
-                        question.setSelections(backup.drop(3).toTypedArray())
-                        question.type = Constants.SELECT
+                    resultNumber += 1
 
-                        resultNumber += 1
-
-                    }
 
                 } else if (backup[0] == context.getString(R.string.load_selection_auto_problems)) {
 
+                    if (backup.size != 4) continue
+
                     val otherNum = Integer.parseInt(backup[3].substring(0, 1))
 
-                    if (otherNum <= Constants.OTHER_SELECT_MAX) {
+                    if (otherNum > Constants.OTHER_SELECT_MAX) continue
 
-                        question.answer = backup[2]
-                        question.auto = true
-                        question.type = Constants.SELECT
-                        question.setSelections(Array(otherNum) { context.getString(R.string.state_auto) })
+                    question.answer = backup[2]
+                    question.auto = true
+                    question.type = Constants.SELECT
+                    question.setSelections(Array(otherNum) { context.getString(R.string.state_auto) })
 
-                        resultNumber += 1
+                    resultNumber += 1
 
-                    }
+
                 } else if (backup[0] == context.getString(R.string.load_select_complete_auto_problem)) {
 
                     val otherNum = Integer.parseInt(backup[2].substring(0, 1))
 
-                    if (otherNum <= Constants.OTHER_SELECT_MAX) {
+                    if (otherNum > Constants.OTHER_SELECT_MAX) continue
 
-                        val others = Array(otherNum) { context.getString(R.string.state_auto) }
+                    val others = Array(otherNum) { context.getString(R.string.state_auto) }
 
-                        val answers = backup.drop(3).toTypedArray()
+                    val answers = backup.drop(3).toTypedArray()
 
-                        if (others.size + answers.size > Constants.SELECT_COMPLETE_MAX) continue //要素数オーバー
+                    if (others.size + answers.size > Constants.SELECT_COMPLETE_MAX) continue //要素数オーバー
 
-                        question.type = Constants.SELECT_COMPLETE
-                        answers.forEach { question.answer += "$it " }
-                        question.setAnswers(answers)
-                        question.setSelections(others)
-                        question.auto = true
+                    question.type = Constants.SELECT_COMPLETE
+                    answers.forEach { question.answer += "$it " }
+                    question.setAnswers(answers)
+                    question.setSelections(others)
+                    question.auto = true
 
-                        resultNumber += 1
+                    resultNumber += 1
 
-                    }
 
                 } else if (backup[0] == context.getString(R.string.load_select_complete_problem)) {
 
                     question.type = Constants.SELECT_COMPLETE
-
 
                     val answerNum = Integer.parseInt(backup[2].substring(0, 1))
 
