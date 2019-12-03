b9249bf0e00987741c7c6664056eb2696a63d420
keita <banira0428@gmail.com>
2019-12-03 15:31:05 +0900

    軽微な修正

--- app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/category/CategorizedActivity.kt
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/category/CategorizedActivity.kt
@@ -38,9 +38,6 @@ class CategorizedActivity : ShowTestsActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
 
         if (item.itemId == android.R.id.home) {
 
