fa7acd916333b72c072208455b289c649f597331
keita <banira0428@gmail.com>
2020-03-21 09:41:08 +0900

    新規Activityの作成

---
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/edit/EditQuestionActivity.kt
@@ -0,0 +1,11 @@
+package jp.gr.java_conf.foobar.testmaker.service.view.edit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditQuestionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
