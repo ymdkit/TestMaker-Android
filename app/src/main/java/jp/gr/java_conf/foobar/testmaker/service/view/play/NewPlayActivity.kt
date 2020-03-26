a3743e42297972e6f95219aa8179b24a9c11036a
keita <banira0428@gmail.com>
2020-03-26 22:42:53 +0900

    キーボードの動的開閉機能実装

--- app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/play/NewPlayActivity.kt
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/play/NewPlayActivity.kt
@@ -1,9 +1,12 @@
 package jp.gr.java_conf.foobar.testmaker.service.view.play
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
+import jp.gr.java_conf.foobar.testmaker.service.Constants
 import jp.gr.java_conf.foobar.testmaker.service.R
 import jp.gr.java_conf.foobar.testmaker.service.databinding.ActivityNewPlayBinding
 import jp.gr.java_conf.foobar.testmaker.service.domain.Test
@@ -13,6 +16,8 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NewPlayActivity : BaseActivity() {
 
+    private val inputMethodManager by lazy { getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager }
+
     private val playViewModel: NewPlayViewModel by viewModel()
     private val testViewModel: TestViewModel by viewModel()
 
@@ -20,13 +25,17 @@ class NewPlayActivity : BaseActivity() {
 
     private lateinit var test: Test
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val binding = DataBindingUtil.setContentView<ActivityNewPlayBinding>(this, R.layout.activity_new_play).apply {
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityNewPlayBinding>(this, R.layout.activity_new_play).apply {
             lifecycleOwner = this@NewPlayActivity
             viewModel = playViewModel
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
 
         createAd(binding.adView)
 
@@ -38,7 +47,26 @@ class NewPlayActivity : BaseActivity() {
             supportActionBar?.setTitle(test.title)
         }
 
+        binding.editAnswer.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                inputMethodManager.showSoftInput(v, InputMethodManager.SHOW_FORCED)
+            } else {
+                inputMethodManager.hideSoftInputFromWindow(v.windowToken, 0)
+            }
+        }
+
         playViewModel.selectedQuestion.value = test.questions[0]
+    }
+
+    override fun onStart() {
+        super.onStart()
+        playViewModel.selectedQuestion.value?.let {
+            when (it.type) {
+                Constants.WRITE -> binding.editAnswer.requestFocus()
+                else -> {
+                }
+            }
+        }
     }
 
     companion object {
