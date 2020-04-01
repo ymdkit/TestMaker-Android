d76bcd5f5607cda5e2ba708535efbc1ef3353523
keita <banira0428@gmail.com>
2020-04-02 08:59:52 +0900

    不要コードの削除

--- app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/category/EditCategoryActivity.kt
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/category/EditCategoryActivity.kt
@@ -1,11 +1,9 @@
 package jp.gr.java_conf.foobar.testmaker.service.view.category
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import jp.gr.java_conf.foobar.testmaker.service.R
 import jp.gr.java_conf.foobar.testmaker.service.databinding.ActivityEditCategoryBinding
@@ -14,8 +12,6 @@ import jp.gr.java_conf.foobar.testmaker.service.view.share.BaseActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class EditCategoryActivity : BaseActivity() {
-    private val inputMethodManager by lazy { getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager }
-
     private val editCategoryViewModel: EditCategoryViewModel by viewModel()
     private val categoryViewModel: CategoryViewModel by viewModel()
 
@@ -32,14 +28,6 @@ class EditCategoryActivity : BaseActivity() {
         createAd(binding.adView)
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        binding.editTitle.setOnFocusChangeListener { v, hasFocus ->
-            if (hasFocus) {
-                inputMethodManager.showSoftInput(v, InputMethodManager.SHOW_FORCED)
-            } else {
-                inputMethodManager.hideSoftInputFromWindow(v.windowToken, 0)
-            }
-        }
 
         binding.buttonAdd.setOnClickListener {
             sendFirebaseEvent("add-category")
