d4dcd7283f3282f47b8f0440530fe738aa1f3647
keita <banira0428@gmail.com>
2020-04-11 12:25:27 +0900

    例外検知用の拡張関数作成

--- app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/extensions/ContextExtension.kt
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/extensions/ContextExtension.kt
@@ -1,8 +1,23 @@
 package jp.gr.java_conf.foobar.testmaker.service.extensions
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
+import jp.gr.java_conf.foobar.testmaker.service.R
+import java.net.UnknownHostException
 
 fun Context.showToast(message: String, length: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, length).show()
+}
+
+fun Context.showErrorToast(e: Throwable, length: Int = Toast.LENGTH_SHORT) {
+    when (e) {
+        is UnknownHostException -> {
+            Toast.makeText(this, getString(R.string.network_error), length).show()
+        }
+        else -> {
+            Toast.makeText(this, getString(R.string.error), length).show()
+            Log.e("ERROR", e.javaClass.simpleName)
+        }
+    }
 }
