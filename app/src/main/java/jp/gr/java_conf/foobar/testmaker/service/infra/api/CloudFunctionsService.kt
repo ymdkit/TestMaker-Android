d2c9bbb48587f79d5e3df81a4f7802b5703786be
keita <banira0428@gmail.com>
2020-04-11 10:31:45 +0900

    通信用モジュールの追加

---
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/infra/api/CloudFunctionsService.kt
@@ -0,0 +1,14 @@
+package jp.gr.java_conf.foobar.testmaker.service.infra.api
+
+import jp.gr.java_conf.foobar.testmaker.service.domain.Test
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface CloudFunctionsService {
+
+    @FormUrlEncoded
+    @POST("textToTest")
+    suspend fun textToTest(@Field("text") text: String): Test
+
+}
