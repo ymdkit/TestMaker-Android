6b1d889d93e250068e473f14f4f28e0069146c8f
山田敬汰 <banira0428@gmail.com>
2021-03-13 08:54:43 +0900

    fix: build.gradle（project） の kts 化

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    val kotlin_version by extra("1.3.72")
+    val nav_version by extra("2.3.3")
+    repositories {
+        jcenter()
+        google()
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("io.realm:realm-gradle-plugin:6.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("com.google.gms:google-services:4.3.5")
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.2")
+        classpath("gradle.plugin.com.github.konifar.gradle:plugin:0.3.3")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.4.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        jcenter()
+        google()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
