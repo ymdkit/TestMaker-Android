4c5fe5a23f1ec9d6c9c042d29d7f04398345c22f
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2021-06-06 12:10:49 +0900

    Update kotlin

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -3,7 +3,7 @@ object Deps {
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
 
-    const val kotlinVersion = "1.4.30"
+    const val kotlinVersion = "1.4.32"
     const val fragmentVersion = "1.3.2"
     const val navVersion = "2.3.3"
 }
