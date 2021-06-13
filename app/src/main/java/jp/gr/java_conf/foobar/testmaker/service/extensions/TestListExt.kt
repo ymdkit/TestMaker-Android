bb624bc85b8236268dbd5031116ef4b42d817f7f
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2021-06-13 22:44:00 +0900

    Rmv: 未使用の拡張関数を削除

--- app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/extensions/TestListExt.kt
+++
@@ -1,9 +0,0 @@
-package jp.gr.java_conf.foobar.testmaker.service.extensions
-
-import jp.gr.java_conf.foobar.testmaker.service.domain.RealmTest
-
-fun List<RealmTest>.getTestsWithCategory(category: String): Int {
-
-    return this.count { it.getCategory() == category }
-
-}
