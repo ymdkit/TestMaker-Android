4c884e6b06103973c777096490535723fb1a08a0
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2021-06-22 10:28:56 +0900

    Upd: テーマカラーの変更

--- app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/result/ui/theme/Theme.kt
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/result/ui/theme/Theme.kt
@@ -5,18 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0XFF0099CC),
+    secondary = Color(0XFFBA7827),
+    surface = Color(0X00000000),
+    onPrimary = Color.White,
+    onSurface = Color(0X99FFFFFF),
+    onBackground = Color(0X99FFFFFF),
+    onSecondary = Color(0X99FFFFFF),
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
+    primary = Color(0XFF33B5E5),
     /* Other default colors to override
     background = Color.White,
     surface = Color.White,
@@ -28,7 +30,10 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun TestMakerAndroidTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun TestMakerAndroidTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
