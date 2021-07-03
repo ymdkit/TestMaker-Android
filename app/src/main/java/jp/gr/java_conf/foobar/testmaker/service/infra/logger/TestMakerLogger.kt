ffb9ce4b3fd503d0741bda94af73e5dd161d35a9
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2021-07-03 15:51:43 +0900

    Fix: ログのキーの誤りを修正

--- app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/infra/logger/TestMakerLogger.kt
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/infra/logger/TestMakerLogger.kt
@@ -2,7 +2,6 @@ package jp.gr.java_conf.foobar.testmaker.service.infra.logger
 
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.logEvent
-import jp.gr.java_conf.foobar.testmaker.service.domain.Question
 
 class TestMakerLogger(private val analytics: FirebaseAnalytics) {
 
@@ -18,23 +17,9 @@ class TestMakerLogger(private val analytics: FirebaseAnalytics) {
         }
 
     fun logAnsweredTestEvent(title: String, count: Int) =
-        analytics.logEvent("create_test"){
+        analytics.logEvent("answered_test"){
             param(FirebaseAnalytics.Param.ITEM_NAME, title)
             param("count", count.toLong())
-        }
-
-
-    fun logCreateQuestionEvent(question: Question) =
-        analytics.logEvent("create_question"){
-            param("question", question.question)
-            param("answer", question.answer)
-            param("answers", question.answers.joinToString(","))
-            param("others", question.others.joinToString(","))
-            param("is_auto", question.isAutoGenerateOthers.toString())
-            param("is_check_complete", question.isCheckOrder.toString())
-            param("is_use_image", question.imagePath.isNotEmpty().toString())
-            param("type", question.type.toString())
-            param("explanation", question.explanation)
         }
 
     fun logEvent(eventName: String) = analytics.logEvent(eventName){}
