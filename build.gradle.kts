9ea377dba3acd32f0a7768580983da0d1c17fdd7
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-08-04 11:53:23 +0900

    Upd: Compose 関連のライブラリのアップデート

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta04")
+        classpath("com.android.tools.build:gradle:7.0.0")
         classpath("io.realm:realm-gradle-plugin:6.0.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
         classpath("com.google.gms:google-services:4.3.5")
