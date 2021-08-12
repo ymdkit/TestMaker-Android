d871d526f4cef0b45d2e968a6eb38bad3ae77387
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-08-12 10:46:45 +0900

    Upd: ライブラリのアップデート

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,11 +97,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.compose.ui:ui:1.0.0")
     implementation("androidx.compose.ui:ui-tooling:1.0.0")
