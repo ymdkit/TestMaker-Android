b867dff11f096c499fcd7dc92b6f754d0665fa93
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-09-27 22:34:35 +0900

    Add: みんなの問題集の Compose 化着手

---
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/online/PublicTestsActivity.kt
@@ -0,0 +1,118 @@
+package jp.gr.java_conf.foobar.testmaker.service.view.online
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ExperimentalGraphicsApi
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import jp.gr.java_conf.foobar.testmaker.service.R
+import jp.gr.java_conf.foobar.testmaker.service.infra.db.SharedPreferenceManager
+import jp.gr.java_conf.foobar.testmaker.service.infra.firebase.FirebaseTest
+import jp.gr.java_conf.foobar.testmaker.service.view.main.TestViewModel
+import jp.gr.java_conf.foobar.testmaker.service.view.online.PublicTestsActivity.Companion.COLOR_MAX
+import jp.gr.java_conf.foobar.testmaker.service.view.result.MyTopAppBar
+import jp.gr.java_conf.foobar.testmaker.service.view.share.component.ComposeAdView
+import jp.gr.java_conf.foobar.testmaker.service.view.ui.theme.TestMakerAndroidTheme
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class PublicTestsActivity : ComponentActivity() {
+
+    companion object {
+        const val COLOR_MAX = 8F
+
+        fun startActivity(activity: Activity) =
+            activity.startActivity(
+                Intent(
+                    activity,
+                    PublicTestsActivity::class.java,
+                )
+            )
+    }
+
+    private val viewModel: FirebaseViewModel by viewModel()
+    private val testViewModel: TestViewModel by viewModel()
+    private val sharedPreferenceManager: SharedPreferenceManager by inject()
+
+    @ExperimentalGraphicsApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            val tests by viewModel.tests.observeAsState()
+
+            TestMakerAndroidTheme {
+                Scaffold(
+                    topBar = {
+                        MyTopAppBar(getString(R.string.label_public_tests))
+                    },
+                    content = {
+                        Surface(color = MaterialTheme.colors.surface) {
+                            Column {
+                                Column(
+                                    modifier = Modifier
+                                        .verticalScroll(state = ScrollState(0))
+                                        .weight(weight = 1f, fill = true)
+                                        .padding(top = 16.dp)
+                                ) {
+
+                                    tests?.map {
+                                        ItemPublicTest(it)
+                                    }
+                                }
+
+                                ComposeAdView(isRemovedAd = sharedPreferenceManager.isRemovedAd)
+                            }
+                        }
+                    }
+                )
+            }
+        }
+        viewModel.getTests()
+    }
+}
+
+@ExperimentalGraphicsApi
+@Composable
+fun ItemPublicTest(test: FirebaseTest) {
+    Row(modifier = Modifier
+        .height(64.dp)
+        .padding(16.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_baseline_description_24),
+            contentDescription = "icon test",
+            colorFilter = ColorFilter.tint(
+                Color.Companion.hsv(360F * test.color.toFloat() / COLOR_MAX, 0.5F, 0.9F),
+                BlendMode.SrcIn
+            ),
+            modifier = Modifier
+                .width(24.dp)
+                .height(24.dp)
+        )
+
+        Text(text = test.name,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold, modifier = Modifier.padding(horizontal = 12.dp))
+    }
+}
