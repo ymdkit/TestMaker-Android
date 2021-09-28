eae8ddff250bb1b8d35a0ee9dadd2323d38d0687
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-09-28 22:51:56 +0900

    Upd: 依存関係のアップデート

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,10 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.0.2")
         classpath("io.realm:realm-gradle-plugin:6.0.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-        classpath("com.google.gms:google-services:4.3.5")
+        classpath("com.google.gms:google-services:4.3.10")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.4")
         classpath("gradle.plugin.com.github.konifar.gradle:plugin:0.3.3")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.2")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Deps.navVersion}")
         classpath("co.uzzu.dotenv:gradle:1.1.0")
         // NOTE: Do not place your application dependencies here; they belong
