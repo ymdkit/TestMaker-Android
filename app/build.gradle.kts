9f0062bd5a44c7270691c6abcce7beb7e48be9d7
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-09-28 10:17:26 +0900

    Upd: アプリのバージョンアップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         minSdk = Deps.minSdkVersion
         targetSdk = Deps.targetSdkVersion
         multiDexEnabled = true
-        versionCode = 155
-        versionName = "4.2.0"
+        versionCode = 156
+        versionName = "4.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
