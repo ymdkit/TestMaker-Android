0f16e7281a24304b53931d0d0845b45f05dc475a
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-10-04 22:20:44 +0900

    Add: 新規画面の追加

---
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/edit/EditQuestionsActivity.kt
@@ -0,0 +1,10 @@
+package jp.gr.java_conf.foobar.testmaker.service.view.edit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
