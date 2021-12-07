6dd6ce21879dec640174991893a750638697ca2c
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-12-08 08:18:13 +0900

    Add: リストの要素の置換用の拡張関数を追加

--- app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/extensions/CollectionExt.kt
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/extensions/CollectionExt.kt
@@ -6,3 +6,6 @@ inline fun <T> Iterable<T>.allIndexed(predicate: (index: Int, T) -> Boolean): Bo
     for (element in this) if (!predicate(index++,element)) return false
     return true
 }
+
+fun <T> Iterable<T>.replaced(index: Int, element: T) =
+    mapIndexed{ i, t -> if(index == i) element else t }
