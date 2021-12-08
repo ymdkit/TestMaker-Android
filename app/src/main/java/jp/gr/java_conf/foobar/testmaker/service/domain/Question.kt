77b208c8dcdb3ced4cd5cd35bbcc6af5db743489
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-12-09 08:34:51 +0900

    Rmv: 未使用関数の削除

--- app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/domain/Question.kt
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/domain/Question.kt
@@ -2,12 +2,10 @@ package jp.gr.java_conf.foobar.testmaker.service.domain
 
 import android.os.Parcelable
 import jp.gr.java_conf.foobar.testmaker.service.Constants
-import jp.gr.java_conf.foobar.testmaker.service.extensions.allIndexed
 import jp.gr.java_conf.foobar.testmaker.service.infra.api.QuestionResponse
 import jp.gr.java_conf.foobar.testmaker.service.infra.firebase.FirebaseQuestion
 import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
-import java.util.*
 
 @Parcelize
 data class Question(
@@ -26,40 +24,6 @@ data class Question(
         var isCheckOrder: Boolean = false,
         var documentId: String = ""
 ) : Parcelable {
-
-    fun getReversibleAnswer(isReverse: Boolean = false) = if (isReversible() && isReverse) question else answer
-
-    fun getAnswerForReview() =
-            when (type) {
-                Constants.WRITE, Constants.SELECT -> answer
-                Constants.COMPLETE, Constants.SELECT_COMPLETE -> answers.joinToString("\n")
-                else -> answer
-            }
-
-
-    fun isCorrect(yourAnswer: String, isReverse: Boolean, isCaseInsensitive: Boolean): Boolean =
-            if (isCaseInsensitive)
-                yourAnswer.lowercase(Locale.ENGLISH) == this.getReversibleAnswer(isReverse)
-                    .lowercase(Locale.ENGLISH)
-            else
-                yourAnswer == this.getReversibleAnswer(isReverse)
-
-    fun isCorrect(yourAnswers: List<String>, isCaseInsensitive: Boolean = false): Boolean {
-        val yours = if (isCaseInsensitive) yourAnswers.map { it.lowercase(Locale.ENGLISH) } else yourAnswers
-        val original = if (isCaseInsensitive) answers.map { it.lowercase(Locale.ENGLISH) } else answers
-
-        if (yours.size != original.size) return false
-
-        if (isCheckOrder) {
-            if (!yours.allIndexed { index, it -> it == original[index] }) return false
-        } else {
-            if (!yours.all { yourAnswer -> original.map { it }.contains(yourAnswer) }) return false
-            if (yours.distinct().size != original.size) return false
-        }
-        return true
-    }
-
-    private fun isReversible() = type == Constants.WRITE || type == Constants.COMPLETE
 
     fun toFirebaseQuestion(imageUrl: String = "") = FirebaseQuestion(
         question = question,
