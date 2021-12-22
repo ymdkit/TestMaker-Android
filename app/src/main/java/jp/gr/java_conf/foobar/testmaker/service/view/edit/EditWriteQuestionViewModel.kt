0d29e03c4215e4de80bef81af0c7be27750acfcc
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-12-22 14:16:31 +0900

    Revert "Rmv: 旧問題編集画面の ViewModel, レイアウトファイルを削除"
    This reverts commit 4a01b600

---
+++ app/src/main/java/jp/gr/java_conf/foobar/testmaker/service/view/edit/EditWriteQuestionViewModel.kt
@@ -0,0 +1,46 @@
+package jp.gr.java_conf.foobar.testmaker.service.view.edit
+
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import jp.gr.java_conf.foobar.testmaker.service.Constants
+import jp.gr.java_conf.foobar.testmaker.service.domain.Question
+import jp.gr.java_conf.foobar.testmaker.service.infra.db.SharedPreferenceManager
+
+class EditWriteQuestionViewModel(preferences: SharedPreferenceManager) : EditQuestionViewModel(preferences) {
+
+    override var selectedQuestion = Question()
+        set(value) {
+            field = value
+            inputForm(field)
+        }
+
+    val answer = MutableLiveData("")
+
+    val isValidated = MediatorLiveData<Boolean>().also { result ->
+        result.addSource(question) { result.value = isValid }
+        result.addSource(answer) { result.value = isValid }
+    }
+
+    private val isValid: Boolean
+        get() = !question.value.isNullOrEmpty() && !answer.value.isNullOrEmpty()
+
+    override fun createQuestion() = selectedQuestion.copy(
+            question = question.value ?: "",
+            answer = answer.value ?: "",
+            explanation = explanation.value ?: "",
+            type = Constants.WRITE,
+            imagePath = imagePath.value ?: ""
+    )
+
+    override fun resetForm() {
+        if (isResetForm.value == true) {
+            super.resetForm()
+            answer.value = ""
+        }
+    }
+
+    override fun inputForm(question: Question) {
+        super.inputForm(question)
+        answer.value = question.answer
+    }
+}
