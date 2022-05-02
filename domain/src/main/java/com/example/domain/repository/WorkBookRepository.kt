6ceca93592a7186e27486fa2a2b7ab98be1a035c
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2022-05-02 17:24:25 +0900

    fix: リポジトリクラスの統合

--- domain/src/main/java/com/example/domain/repository/WorkBookRepository.kt
+++ domain/src/main/java/com/example/domain/repository/WorkBookRepository.kt
@@ -1,18 +1,30 @@
 package com.example.domain.repository
 
+import com.example.domain.model.Folder
+import com.example.domain.model.FolderId
 import com.example.domain.model.Workbook
 import com.example.domain.model.WorkbookId
 import kotlinx.coroutines.flow.Flow
 
 interface WorkBookRepository {
 
+    val createFolderFlow: Flow<Folder>
+    val updateFolderFlow: Flow<Folder>
+    val deleteFolderFlow: Flow<FolderId>
     val createWorkbookFlow: Flow<Workbook>
     val updateWorkbookFlow: Flow<Workbook>
     val deleteWorkbookFlow: Flow<WorkbookId>
 
+    suspend fun getFolderList(): List<Folder>
+    suspend fun getFolder(folderId: FolderId): Folder
+    suspend fun createFolder(folder: Folder)
+    suspend fun updateFolder(folder: Folder)
+    suspend fun deleteFolder(folderId: FolderId)
     suspend fun getWorkbookList(): List<Workbook>
     suspend fun getWorkbook(workbookId: WorkbookId): Workbook
     suspend fun createWorkbook(workbook: Workbook)
     suspend fun updateWorkbook(workbook: Workbook)
     suspend fun deleteWorkbook(workbookId: WorkbookId)
+
+    suspend fun getWorkbookListByFolderName(folderName: String): List<Workbook>
 }
--- infra/src/main/java/com/example/infra/repository/WorkbookRepositoryImpl.kt
+++ infra/src/main/java/com/example/infra/repository/WorkbookRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package com.example.infra.repository
 
+import com.example.domain.model.Folder
+import com.example.domain.model.FolderId
 import com.example.domain.model.Workbook
 import com.example.domain.model.WorkbookId
 import com.example.domain.repository.WorkBookRepository
+import com.example.infra.local.db.FolderDataSource
 import com.example.infra.local.db.WorkbookDataSource
+import com.example.infra.local.entity.RealmCategory
 import com.example.infra.local.entity.RealmTest
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -12,8 +16,24 @@ import javax.inject.Singleton
 
 @Singleton
 class WorkbookRepositoryImpl @Inject constructor(
-    private val db: WorkbookDataSource,
+    private val workbookDataSource: WorkbookDataSource,
+    private val folderDataSource: FolderDataSource,
 ) : WorkBookRepository {
+
+    private val _createFolderFlow: MutableSharedFlow<Folder> =
+        MutableSharedFlow()
+    override val createFolderFlow: Flow<Folder>
+        get() = _createFolderFlow
+
+    private val _updateFolderFlow: MutableSharedFlow<Folder> =
+        MutableSharedFlow()
+    override val updateFolderFlow: Flow<Folder>
+        get() = _updateFolderFlow
+
+    private val _deleteFolderFlow: MutableSharedFlow<FolderId> =
+        MutableSharedFlow()
+    override val deleteFolderFlow: Flow<FolderId>
+        get() = _deleteFolderFlow
 
     private val _createWorkbookFlow: MutableSharedFlow<Workbook> =
         MutableSharedFlow()
@@ -30,27 +50,59 @@ class WorkbookRepositoryImpl @Inject constructor(
     override val deleteWorkbookFlow: Flow<WorkbookId>
         get() = _deleteWorkbookFlow
 
+    override suspend fun getFolderList(): List<Folder> {
+        return folderDataSource.getFolderList().map {
+            val workbookCount = getWorkbookListByFolderName(folderName = it.name).count()
+            it.toFolder(workbookCount = workbookCount)
+        }
+    }
+
+    override suspend fun getFolder(folderId: FolderId): Folder {
+        val realmFolder = folderDataSource.getFolder(folderId = folderId.value)
+        val workbookCount = getWorkbookListByFolderName(folderName = realmFolder.name).count()
+        return realmFolder.toFolder(workbookCount = workbookCount)
+    }
+
+    override suspend fun createFolder(folder: Folder) {
+        folderDataSource.createFolder(RealmCategory.fromFolder(folder))
+        _createFolderFlow.emit(folder)
+    }
+
+    override suspend fun updateFolder(folder: Folder) {
+        folderDataSource.createFolder(RealmCategory.fromFolder(folder))
+        _updateFolderFlow.emit(folder)
+    }
+
+    override suspend fun deleteFolder(folderId: FolderId) {
+        folderDataSource.deleteFolder(folderId.value)
+        _deleteFolderFlow.emit(folderId)
+    }
+
     override suspend fun getWorkbookList(): List<Workbook> =
-        db.getWorkbookList().map {
+        workbookDataSource.getWorkbookList().map {
             it.toWorkbook()
         }
 
     override suspend fun getWorkbook(workbookId: WorkbookId): Workbook =
-        db.getWorkbook(id = workbookId.value).toWorkbook()
+        workbookDataSource.getWorkbook(id = workbookId.value).toWorkbook()
 
     override suspend fun createWorkbook(workbook: Workbook) {
-        db.createWorkbook(RealmTest.fromWorkbook(workbook))
+        this.workbookDataSource.createWorkbook(RealmTest.fromWorkbook(workbook))
         _createWorkbookFlow.emit(workbook)
     }
 
     override suspend fun updateWorkbook(workbook: Workbook) {
-        db.createWorkbook(RealmTest.fromWorkbook(workbook))
+        this.workbookDataSource.createWorkbook(RealmTest.fromWorkbook(workbook))
         _updateWorkbookFlow.emit(workbook)
     }
 
     override suspend fun deleteWorkbook(workbookId: WorkbookId) {
-        db.deleteWorkbook(workbookId.value)
+        workbookDataSource.deleteWorkbook(workbookId.value)
         _deleteWorkbookFlow.emit(workbookId)
     }
 
+    override suspend fun getWorkbookListByFolderName(folderName: String): List<Workbook> =
+        workbookDataSource.getWorkbookList()
+            .filter { workbook -> workbook.getCategory() == folderName }
+            .map { it.toWorkbook() }
 }
