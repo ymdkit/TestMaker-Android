1fd13ae850ce3626be690557e6d37efd06a84dd5
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2022-05-02 17:26:53 +0900

    feat: Folder を削除するための関数を追加

--- usecase/src/main/java/com/example/usecase/UserCommandUseCase.kt
+++ usecase/src/main/java/com/example/usecase/UserCommandUseCase.kt
@@ -1,7 +1,9 @@
 package com.example.usecase
 
+import com.example.domain.model.FolderId
 import com.example.domain.model.WorkbookId
 import com.example.domain.repository.WorkBookRepository
+import com.example.usecase.model.FolderUseCaseModel
 import com.example.usecase.model.WorkbookUseCaseModel
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,6 +12,16 @@ import javax.inject.Singleton
 class UserCommandUseCase @Inject constructor(
     private val workBookRepository: WorkBookRepository
 ) {
+
+    suspend fun deleteFolder(folder: FolderUseCaseModel) {
+        val folderName = folder.name
+        val workbookList = workBookRepository.getWorkbookListByFolderName(folderName)
+        workBookRepository.deleteFolder(FolderId(folder.id))
+        workbookList.forEach {
+            workBookRepository.deleteWorkbook(it.id)
+        }
+    }
+
     suspend fun deleteWorkbook(workbook: WorkbookUseCaseModel) =
         workBookRepository.deleteWorkbook(WorkbookId(workbook.id))
 }
