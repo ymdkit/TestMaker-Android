4ff6aa5c26e7233f3020e556bf68f01a5d16a6f0
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2022-05-02 13:41:41 +0900

    feat: Resource を結合させるための関数を追加

--- usecase/src/main/java/com/example/usecase/utils/Resource.kt
+++ usecase/src/main/java/com/example/usecase/utils/Resource.kt
@@ -6,6 +6,27 @@ sealed class Resource<out T> {
     data class Success<T>(val value: T) : Resource<T>()
     data class Failure(val message: String) : Resource<Nothing>()
 
+    companion object {
+        fun <T, U, V> merge(
+            resource1: Resource<T>,
+            resource2: Resource<U>,
+            transform: (T, U) -> V
+        ): Resource<V> =
+            when {
+                resource1 is Failure && resource2 is Failure -> Failure(message = "${resource1.message}, ${resource2.message}")
+                resource1 is Failure -> Failure(message = resource1.message)
+                resource2 is Failure -> Failure(message = resource2.message)
+                resource1 is Loading || resource2 is Loading -> Loading
+                resource1 is Success && resource2 is Success -> Success(
+                    value = transform(
+                        resource1.value,
+                        resource2.value
+                    )
+                )
+                else -> Empty
+            }
+    }
+
     fun <U> map(block: (T) -> U): Resource<U> =
         when (this) {
             is Success -> Success(block(this.value))
