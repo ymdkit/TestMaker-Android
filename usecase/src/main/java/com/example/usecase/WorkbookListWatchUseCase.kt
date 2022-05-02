f69bb017646e3cfcc913fcc393a3f6b21341661d
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2022-05-02 13:45:41 +0900

    feat: 問題集に関する操作を監視できるようにする

---
+++ usecase/src/main/java/com/example/usecase/WorkbookListWatchUseCase.kt
@@ -0,0 +1,64 @@
+package com.example.usecase
+
+import com.example.domain.repository.WorkBookRepository
+import com.example.usecase.model.WorkbookUseCaseModel
+import com.example.usecase.utils.Resource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class WorkbookListWatchUseCase @Inject constructor(
+    private val repository: WorkBookRepository
+) {
+
+    private val _flow: MutableStateFlow<Resource<List<WorkbookUseCaseModel>>> =
+        MutableStateFlow(Resource.Empty)
+    val flow: StateFlow<Resource<List<WorkbookUseCaseModel>>> = _flow
+
+    fun setup(scope: CoroutineScope) {
+        scope.launch {
+
+            repository.createWorkbookFlow
+                .onEach {
+                    val new = _flow.value.map { list ->
+                        list + WorkbookUseCaseModel.fromWorkbook(it)
+                    }
+                    _flow.emit(new)
+                }.launchIn(scope)
+
+            repository.updateWorkbookFlow
+                .onEach { workbook ->
+                    val new = _flow.value.map { list ->
+                        list.map {
+                            if (workbook.id.value == it.id) WorkbookUseCaseModel.fromWorkbook(
+                                workbook
+                            ) else it
+                        }
+                    }
+                    _flow.emit(new)
+                }.launchIn(scope)
+
+            repository.deleteWorkbookFlow
+                .onEach { workbookId ->
+                    val new = _flow.value.map { list ->
+                        list.filter { it.id != workbookId.value }
+                    }
+                    _flow.emit(new)
+                }.launchIn(scope)
+
+        }
+    }
+
+    suspend fun load() {
+        _flow.emit(Resource.Loading)
+        val workbookList = repository.getWorkbookList()
+        _flow.emit(Resource.Success(workbookList.map {
+            WorkbookUseCaseModel.fromWorkbook(it)
+        }))
+    }
+}
+
