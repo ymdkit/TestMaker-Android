014a23083d8011cdd292e95927ebd34c21fd8fd1
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2022-05-04 10:10:18 +0900

    feat: 問題集の更新情報を監視できるようにする

--- usecase/src/main/java/com/example/usecase/WorkbookWatchUseCase.kt
+++ usecase/src/main/java/com/example/usecase/WorkbookWatchUseCase.kt
@@ -4,8 +4,12 @@ import com.example.domain.model.WorkbookId
 import com.example.domain.repository.WorkBookRepository
 import com.example.usecase.model.WorkbookUseCaseModel
 import com.example.usecase.utils.Resource
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.properties.Delegates
@@ -21,8 +25,17 @@ class WorkbookWatchUseCase @Inject constructor(
 
     private var workbookId by Delegates.notNull<Long>()
 
-    fun setup(workbookId: Long) {
+    fun setup(workbookId: Long, scope: CoroutineScope) {
         this.workbookId = workbookId
+
+        scope.launch {
+            repository.updateWorkBookListFlow
+                .onEach { list ->
+                    val newWorkbook = list.find { it.id.value == workbookId } ?: return@onEach
+                    _flow.emit(_flow.value.map { WorkbookUseCaseModel.fromWorkbook(newWorkbook) })
+                }
+                .launchIn(this)
+        }
     }
 
     suspend fun load() {
